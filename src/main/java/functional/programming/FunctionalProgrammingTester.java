82b0c36685488a65ac83503609e81795df48132b
amitabhmandal <amitabhmandal@192.168.1.12>
2018-07-06 10:14:16 +0530

    added comment for Java 10

--- src/main/java/functional/programming/FunctionalProgrammingTester.java
+++ src/main/java/functional/programming/FunctionalProgrammingTester.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class FunctionalProgrammingTester {
 
     public static void main(String[] args) {
-        List<String> list = List.of("Apple", "Banana", "Cat", "Dog", "bat");
+        /*  List<String> list = List.of("Apple", "Banana", "Cat", "Dog", "bat");
         printBasic(list);
         printBasicFP(list);
         printBasicWithFiltering(list);
@@ -17,10 +17,10 @@ public class FunctionalProgrammingTester {
         
         
         basicSumOfNumberWithFunctionalProgramming(listNumber);
-
-    }
-
-    private static int basicSumOfNumber(List<Integer> list) {
+        
+        }
+        
+        private static int basicSumOfNumber(List<Integer> list) {
         int sum = 0;
         System.out.println("Addition of Numbers Without functional Programming call");
         for (Integer integer : list) {
@@ -28,56 +28,56 @@ public class FunctionalProgrammingTester {
             sum = sum + integer;
         }
         return sum;
-    }
-    
-    private static void basicSumOfNumberWithFunctionalProgramming(List<Integer> list) {
+        }
+        
+        private static void basicSumOfNumberWithFunctionalProgramming(List<Integer> list) {
         System.out.println("Addition of Numbers With functional Programming call");
         
         System.out.println(list.stream().reduce(0, (number1, number2 ) -> number1 + number2));
-    }
-
-    
-    
-    private static void printBasic(List<String> list) {
+        }
+        
+        
+        
+        private static void printBasic(List<String> list) {
         System.out.println("Without functional Programming call");
         for (String string : list) {
             System.out.println(string);
         }
-    }
-
-    private static void printBasicWithFiltering(List<String> list) {
+        }
+        
+        private static void printBasicWithFiltering(List<String> list) {
         System.out.println("Without functional Programming call Filtering");
         for (String string : list) {
             if (string.endsWith("at")) {
                 System.out.println(string);
             }
         }
-    }
-
-    private static void printBasicFP(List<String> list) {
+        }
+        
+        private static void printBasicFP(List<String> list) {
         // for(String string : list) {
         // System.out.println(string);
         // }
-
+        
         // Here action is a method.
-
+        
         // Within the for-each we are not sending data but it is a function call.
         // Hence we are sending a function as parameter
-
+        
         System.out.println("With functional Programming call");
-
+        
         list.stream()
             .forEach(element -> System.out.println(element));
-
-    }
-
-    private static void printBasicFPWithFiltering(List<String> list) {
-
+        
+        }
+        
+        private static void printBasicFPWithFiltering(List<String> list) {
+        
         System.out.println("With functional Programming call filtering");
-
+        
         list.stream()
             .filter(element -> element.endsWith("at"))
-            .forEach(element -> System.out.println(element));
+            .forEach(element -> System.out.println(element));*/
 
     }
 }
